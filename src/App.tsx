d6e18c8bd9f475c9b1afe3907b26feec2a7c7d56
mohit191 <notsotypicaldeveloper@gmail.com>
2024-12-25 19:11:08 +0530

    configure app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 import {Hero} from "./components/section/Hero";
 import Skills from "./components/section/Skills";
 import Experience from "./components/section/Experience";
+import StarCanvas from "./components/canvas/Stars";
 
 const Body = styled.div`
   background-color: ${({theme})=> theme.bg};
@@ -15,18 +16,44 @@ const Body = styled.div`
   position: relative;
 `;
 
+const Wrapper = styled.div`
+  padding-bottom: 100px;
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 function App() {
 
   return (
     <>
       <ThemeProvider theme={darkTheme} >
         <BrowserRouter>
-          <Navbar />
-          <Body>
-              <Hero />
+        <Navbar />
+        <Body>
+          <StarCanvas />
+          <div>
+            <Hero />
+            <Wrapper>
               <Skills />
               <Experience />
-          </Body>
+            </Wrapper>
+            {/* <Projects />
+            <Wrapper>
+              <Contact />
+            </Wrapper>
+            <Footer /> */}
+          </div>
+        </Body>
         </BrowserRouter>
       </ThemeProvider>
     </>
